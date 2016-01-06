91d6eb320594555851922fbee650989a8d2339d8
Yusuke Ito <novi.mad@gmail.com>
2016-01-07 01:27:04 +0900

    refactor routes add httpie request examples

--- todoapi/todoapi/main.swift
+++ todoapi/todoapi/main.swift
@@ -27,11 +27,20 @@ app.use(Logger())
 
 app.use(BodyParser())
 
-app.use(Mount("/user", compose(
-    Route("/sum", SumController()),
-    AuthUser(authAs: "user") >>> UserController()
-    )
-))
+// http --verbose POST localhost:3000/user/sum x-auth:uuuu a:=1 b:=2 -> 200 OK
+// http --verbose POST localhost:3000/user/sum x-auth:uuuu a:=1 -> Bad Request
+// http --verbose PUT localhost:3000/user/sum x-auth:uuuu a:=1 b:=2 (using MySQL)
+app.use(Mount("/user", Route("/sum", SumController())))
+
+// http --verbose POST localhost:3000/user x-auth:user a:=1 b:=2 -> 200 OK
+// http --verbose POST localhost:3000/user x-auth:uuuu a:=1 b:=2 -> Forbidden
+app.use(Mount("/user", AuthUser(authAs: "user") >>> UserController()))
+
+
+// http --verbose POST localhost:3000/private/something x-auth:private a:=1 b:=2 -> 200 OK
+// http --verbose POST localhost:3000/private/something x-auth:pppp a:=1 b:=2 -> 200 OK
+// http --verbose POST localhost:3000/private/1234 x-auth:private a:=1 b:=2 -> 200 OK
+// http --verbose POST localhost:3000/private/1234 x-auth:pppp a:=1 b:=2 -> Forbidden
 app.use(Mount("/private", compose(
     Route("/something", PrivateController(name: "something") ),
     Route("/:id", AuthUser(authAs: "private") >>> PrivateController(name: "by id"))
