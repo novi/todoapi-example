85fc988183510add1f0e3eb933330409f52d2d37
Yusuke Ito <novi.mad@gmail.com>
2016-01-15 20:37:32 +0900

    add Nest framework

---
+++ OSX Projects/Nest/Nest/Nest.h
@@ -0,0 +1,19 @@
+//
+//  Nest.h
+//  Nest
+//
+//  Created by Yusuke Ito on 1/15/16.
+//  Copyright © 2016 Yusuke Ito. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Nest.
+FOUNDATION_EXPORT double NestVersionNumber;
+
+//! Project version string for Nest.
+FOUNDATION_EXPORT const unsigned char NestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nest/PublicHeader.h>
+
+
